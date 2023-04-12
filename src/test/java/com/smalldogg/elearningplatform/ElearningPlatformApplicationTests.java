d03d306dfdf160aea663ef5602f1b432c2c8aa0c
small-dogg <sy920112@naver.com>
2023-04-13 01:20:55 +0900

    chore: init

---
+++ src/test/java/com/smalldogg/elearningplatform/ElearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smalldogg.elearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
