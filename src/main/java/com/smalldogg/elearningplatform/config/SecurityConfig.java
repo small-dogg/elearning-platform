e8f0e6a1e7e6e2ae9156fb0c8b411cd4a012e095
small-dogg <sy920112@naver.com>
2023-04-14 10:15:34 +0900

    feat: Security 예외 설정

---
+++ src/main/java/com/smalldogg/elearningplatform/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.smalldogg.elearningplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+            "/", "/user/**", "index.html", "/css/**","/js/**","/assets/**"
+    };
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .shouldFilterAllDispatcherTypes(false)
+                        .requestMatchers(AUTH_WHITELIST)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .build();
+    }
+}
